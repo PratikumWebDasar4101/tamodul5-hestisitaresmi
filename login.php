d4e9507b1f10ecfd1dfdf522d38bf603171069ed
hestisitaresmi <42725278+hestisitaresmi@users.noreply.github.com>
2018-10-01 21:43:58 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,25 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+	<table align="center">
+		<form action="form.php">
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="text"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="Password" name="Password"></td>
+			</tr>
+			<tr>
+				<td align="center" colspan="3"><input type="submit" name="submit" value="KIRIM"></td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
